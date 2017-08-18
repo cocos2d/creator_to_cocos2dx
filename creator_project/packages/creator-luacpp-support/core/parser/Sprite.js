1f6711acedb90405bf2355c841ed74689c47d4b5
minggo <ming@cocos2d-x.org>
2017-08-18 14:10:13 +0800

    fix parse crash if Sprite doesn't have a sprite frame

--- creator_project/packages/creator-luacpp-support/core/parser/Sprite.js
+++ creator_project/packages/creator-luacpp-support/core/parser/Sprite.js
@@ -14,16 +14,19 @@ class Sprite extends Node {
         this._properties = {node: this._properties};
 
         let component = Node.get_node_component_of_type(this._node_data, 'cc.Sprite');
-        let sprite_frame_uuid = component._spriteFrame.__uuid__;
-        
-        name = get_sprite_frame_name_by_uuid(sprite_frame_uuid);
-        if (name) {
-            this._properties['spriteFrameName'] = name
-            this._properties.spriteType = Sprite.SPRITE_TYPES[component._type];
-            this.add_property_int('srcBlend', '_srcBlendFactor', component);
-            this.add_property_int('dstBlend', '_dstBlendFactor', component);
-            this.add_property_bool('trimEnabled', '_isTrimmedMode', component);
-            this._properties.sizeMode = Sprite.SIZE_MODES[component._sizeMode];
+
+        if (component._spriteFrame) {
+            let sprite_frame_uuid = component._spriteFrame.__uuid__;
+            
+            name = get_sprite_frame_name_by_uuid(sprite_frame_uuid);
+            if (name) {
+                this._properties['spriteFrameName'] = name
+                this._properties.spriteType = Sprite.SPRITE_TYPES[component._type];
+                this.add_property_int('srcBlend', '_srcBlendFactor', component);
+                this.add_property_int('dstBlend', '_dstBlendFactor', component);
+                this.add_property_bool('trimEnabled', '_isTrimmedMode', component);
+                this._properties.sizeMode = Sprite.SIZE_MODES[component._sizeMode];
+            }
         }
     }
 }
