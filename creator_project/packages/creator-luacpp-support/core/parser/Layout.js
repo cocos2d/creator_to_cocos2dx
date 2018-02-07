4c8c084c0ff49a57be8e043012aa148c0d4d500f
tankorsmash <tankorsmash@gmail.com>
2018-02-07 17:21:28 -0500

    start on layout child inclusion

--- creator_project/packages/creator-luacpp-support/core/parser/Layout.js
+++ creator_project/packages/creator-luacpp-support/core/parser/Layout.js
@@ -12,6 +12,8 @@ class Layout extends Node {
         super.parse_properties();
 
         let spr_component = Node.get_node_component_of_type(this._node_data, 'cc.Sprite');
+        let lay_component = Node.get_node_component_of_type(this._node_data, 'cc.Layout');
+
         this._properties = {node: this._properties};
     }
 }
